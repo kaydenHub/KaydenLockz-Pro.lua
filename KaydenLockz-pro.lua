8f2bf4cb9db6f57e6dbf1ac4a485ca6af298e7bb
kaydenHub <kaydenhotak@gmail.com>
2025-08-15 09:37:11 -0400

    Create KaydenLockz-pro.lua

---
+++ KaydenLockz-pro.lua
@@ -0,0 +1,11 @@
+--[[
+KaydenLockz Pro - Advanced Combat Lock System v2.1
+Features: Auto-Unlock on death/leave, Stomp detection, Auto-lock on stomp
+Repository: https://github.com/yourusername/KaydenLockz-Pro
+]]
+
+-- Services
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+-- ... [rest of the script remains exactly as previously provided] ...
+print("KaydenLockz Pro v2.1 activated!")
